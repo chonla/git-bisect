4e9729584e4510883c15d355c10f76503a1e9e36
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-10-31 12:09:11 +0700

    Add number matching test

--- spec/file1_spec.js
+++ spec/file1_spec.js
@@ -2,4 +2,8 @@ describe('file 1', function () {
   it('should be okay', function() {
     expect(true).toBeTruthy();
   });
+
+  it('should match number', function() {
+    expect(2).toEqual(2);
+  })
 })
