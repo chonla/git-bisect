c78128215dd28ae1943775f6d34e8ea5ebd5a544
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-10-31 13:45:07 +0700

    Add falsy test

--- spec/file3_spec.js
+++ spec/file3_spec.js
@@ -2,4 +2,8 @@ describe('file 3', function () {
   it('should fail as expected', function() {
     expect(false).toEqual(false);
   });
+
+  it('should be consider as false', function() {
+    expect(0).toBeFalsy();
+  });
 })
