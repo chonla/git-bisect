7040fc569fdad031930621a291771b9219f1c4e1
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-10-31 12:10:19 +0700

    Add truthy test

--- spec/file2_spec.js
+++ spec/file2_spec.js
@@ -2,4 +2,8 @@ describe('file 2', function () {
   it('should be fine', function() {
     expect(true).toEqual(true);
   });
+
+  it('should be considered as true', function() {
+    expect(1).toBeTruthy();
+  });
 })
