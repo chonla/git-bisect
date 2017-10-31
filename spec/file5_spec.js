ed4748dd0f477f3c0f0b4024c2899ea5195daa31
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-10-31 11:42:53 +0700

    Add some math calculation test

---
+++ spec/file5_spec.js
@@ -0,0 +1,14 @@
+function avg(nums) {
+  var out = 0.0;
+  for (var i = 0; i < nums.length; i++) {
+    out += nums[i];
+  }
+  return out / nums.length;
+}
+
+describe('file 5', function () {
+  it('should calculate avg correctly', function () {
+    var result = avg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(result).toEqual(5.5);
+  });
+})
